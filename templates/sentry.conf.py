c1b108ead3cd8d31e3f9bc68856912c49b995c95
Jorge Barnaby <jorge.barnaby@gmail.com>
2014-09-20 14:48:39 -0400

    Sentry config updated. DB and SMTP configurable.

--- templates/sentry.conf.py
+++ templates/sentry.conf.py
@@ -1,4 +1,8 @@
 
+# This file is just Python, with a touch of Django which means you
+# you can inherit and tweak settings to your hearts content.
+from sentry.conf.server import *
+
 import os.path
 
 CONF_ROOT = os.path.dirname(__file__)
@@ -11,37 +15,67 @@ DATABASES = {
 
         # If you change this, you'll also need to install the appropriate python
         # package: psycopg2 (Postgres) or mysql-python
+        #'ENGINE': 'django.db.backends.sqlite3',
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 
-        'NAME': 'sentry',
-        'USER': 'sentry',
-        'PASSWORD': 'sentry',
-        'HOST': '127.0.0.1',
+        #'NAME': os.path.join(CONF_ROOT, 'sentry.db'),
+        'NAME': '{{db_name}}',
+        'USER': '{{db_user}}',
+        'PASSWORD': '{{db_password}}',
+        'HOST': '{{db_host}}',
         'PORT': '',
+
+        # If you're using Postgres, we recommend turning on autocommit
+        'OPTIONS': {
+            'autocommit': True,
+        }
     }
 }
 
-# If you're expecting any kind of real traffic on Sentry, we highly recommend configuring
-# the CACHES and Redis settings
 
+# If you're expecting any kind of real traffic on Sentry, we highly recommend
+# configuring the CACHES and Redis settings
+
+###########
+## CACHE ##
+###########
+
+# You'll need to install the required dependencies for Memcached:
+#   pip install python-memcached
+#
 CACHES = {
-     'default': {
-         'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
-         'LOCATION': ['127.0.0.1:11211'],
-     }
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': ['127.0.0.1:11211'],
+    }
 }
 
-# Buffers (combined with queueing) act as an intermediate layer between the database and
-# the storage API. They will greatly improve efficiency on large numbers of the same events
-# being sent to the API in a short amount of time.
+###########
+## Queue ##
+###########
 
-# SENTRY_USE_QUEUE = True
-# For more information on queue options, see the documentation for Celery:
-# http://celery.readthedocs.org/en/latest/
+# See http://sentry.readthedocs.org/en/latest/queue/index.html for more
+# information on configuring your queue broker and workers. Sentry relies
+# on a Python framework called Celery to manage queues.
+
+# You can enable queueing of jobs by turning off the always eager setting:
+# CELERY_ALWAYS_EAGER = False
 # BROKER_URL = 'redis://localhost:6379'
 
+####################
+## Update Buffers ##
+####################
+
+# Buffers (combined with queueing) act as an intermediate layer between the
+# database and the storage API. They will greatly improve efficiency on large
+# numbers of the same events being sent to the API in a short amount of time.
+# (read: if you send any kind of real data to Sentry, you should enable buffers)
+
+# You'll need to install the required dependencies for Redis buffers:
+#   pip install redis hiredis nydus
+#
 # SENTRY_BUFFER = 'sentry.buffer.redis.RedisBuffer'
-# SENTRY_BUFFER_OPTIONS = {
+# SENTRY_REDIS_OPTIONS = {
 #     'hosts': {
 #         0: {
 #             'host': '127.0.0.1',
@@ -50,32 +84,51 @@ CACHES = {
 #     }
 # }
 
+################
+## Web Server ##
+################
 
-SENTRY_KEY = '{{secret_key}}'
-
-# You should configure the absolute URI to Sentry. It will attempt to guess it if you don't
-# but proxies may interfere with this.
+# You MUST configure the absolute URI root for Sentry:
 SENTRY_URL_PREFIX = '{{root_url}}'  # No trailing slash!
 
-SENTRY_WEB_HOST = '127.0.0.1'
+# If you're using a reverse proxy, you should enable the X-Forwarded-Proto
+# and X-Forwarded-Host headers, and uncomment the following settings
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# USE_X_FORWARDED_HOST = True
+
+SENTRY_WEB_HOST = '0.0.0.0'
 SENTRY_WEB_PORT = 9000
 SENTRY_WEB_OPTIONS = {
     'workers': 3,  # the number of gunicorn workers
+    'limit_request_line': 0,  # required for raven-js
     'secure_scheme_headers': {'X-FORWARDED-PROTO': 'https'},
 }
 
-# Mail server configuration
+#################
+## Mail Server ##
+#################
 
 # For more information check Django's documentation:
 #  https://docs.djangoproject.com/en/1.3/topics/email/?from=olddocs#e-mail-backends
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-SERVER_EMAIL = '{{server_email}}'
+
 EMAIL_HOST = '{{smtp_server}}'
-EMAIL_HOST_PASSWORD = ''
-EMAIL_HOST_USER = ''
-EMAIL_PORT = 25
-EMAIL_USE_TLS = False
+EMAIL_HOST_PASSWORD = '{{smtp_password}}'
+EMAIL_HOST_USER = '{{smtp_user}}'
+EMAIL_PORT = {{smtp_port}}
+EMAIL_USE_TLS = {{smtp_use_tls}}
+
+# The email address to send on behalf of
+SERVER_EMAIL = '{{server_email}}'
+
+###########
+## etc. ##
+###########
+
+# If this file ever becomes compromised, it's important to regenerate your SECRET_KEY
+# Changing this value will result in all current sessions being invalidated
+SECRET_KEY = '{{secret_key}}'
 
 # http://twitter.com/apps/new
 # It's important that input a callback URL, even if its useless. We have no idea why, consult Twitter.
@@ -87,8 +140,8 @@ FACEBOOK_APP_ID = ''
 FACEBOOK_API_SECRET = ''
 
 # http://code.google.com/apis/accounts/docs/OAuth2.html#Registering
-GOOGLE_OAUTH2_CLIENT_ID = '1042684111028.apps.googleusercontent.com'
-GOOGLE_OAUTH2_CLIENT_SECRET = 'tUNMns4mfL4Fb7zLJ3byskLj'
+GOOGLE_OAUTH2_CLIENT_ID = ''
+GOOGLE_OAUTH2_CLIENT_SECRET = ''
 
 # https://github.com/settings/applications/new
 GITHUB_APP_ID = ''
@@ -97,3 +150,8 @@ GITHUB_API_SECRET = ''
 # https://trello.com/1/appKey/generate
 TRELLO_API_KEY = ''
 TRELLO_API_SECRET = ''
+
+# https://confluence.atlassian.com/display/BITBUCKET/OAuth+Consumers
+BITBUCKET_CONSUMER_KEY = ''
+BITBUCKET_CONSUMER_SECRET = ''
+
